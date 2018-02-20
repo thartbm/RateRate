830bff110c329a9d7e2c436c4627fab0e678b369
thartbm <thartbm@gmail.com>
2018-02-20 14:42:08 -0500

    adding documentation for example data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Rotation Adaptation data for use with the two-rate model
+#'
+#' A dataset with a perturbation schedule and the reach direction errors of a group
+#' of participants perofrming an experiment with that schedule.
+#'
+#' @format A data frame with 288 rows and 2 variables:
+#' \describe{
+#'   \item{schedule}{perturbation schedule, for use in the two-rate model}
+#'   \item{reaches}{the median initial reach errors across a group of participants}
+#' }
+"RotAdapt"
